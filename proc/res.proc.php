d63f9f8dc700c4f795853935e3f4e8605dce92e3
Julio-Cesar-Carrillo <100006703.joan23@fje.edu>
2022-02-09 12:39:25 +0100

    first commit

---
+++ proc/res.proc.php
@@ -0,0 +1,23 @@
+<?php 
+// Index a Pantalla1
+if (isset($_POST['index']) && strtolower($_POST['pokemon'])=='pikachu'){
+    // creo una sesión por primera vez.
+    session_start();
+    $_SESSION['pantalla1']='check';
+
+    header('location: ../view/pantalla1.php');
+} else if (isset($_POST['index'])){
+    header('location: ../index.php?msg=25');
+    } 
+// Pantalla1 a Pantalla2
+else if (isset($_POST['pantalla1']) && strtolower($_POST['charmander'])=='charmander'){
+// recupero sesión.
+    session_start();
+    $_SESSION['pantalla2']='check';
+    header('location: ../view/pantalla2.php');
+} else if (isset($_POST['pantalla1'])){
+    header('location: ../view/pantalla1.php?msg=4');
+    session_destroy();
+    header('location: ../index.php');
+} 
+    
---
+++ view/pantalla2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recogida de datos</title>
+</head>
+<body>
+<?php
+    session_start();
+    if (!($_SESSION["pantalla2"]=='check')) {
+        header("location: ../index.php");
+    }
+    ?>
+    <h1>Correcto el pokemon era Charmander</h1>
+    <a href="../proc/replay.proc.php">Volver a jugar</a>
+</body>
+</html>
